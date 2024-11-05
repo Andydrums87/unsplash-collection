d551c29e05b62ff0a4a308c37b9cc5442f6027f3
Andy Joseph <neon_4@hotmail.com>
2024-11-05 12:08:33 +0000

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/unsplash-collection/",
+  build: {
+    outDir: 'dist',
+ },
   server: {  
     host: true  
 }
