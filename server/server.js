6b019fa58cb3e64e8e66435a8be630d3542ef05c
Andy Joseph <neon_4@hotmail.com>
2024-11-07 11:33:57 +0000

    message

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/index.html'), function(err) {
+    res.sendFile(path.join(__dirname, '/..client/dist/index.html'), function(err) {
         if(err) {
             res.status(500).send(err)
         }
