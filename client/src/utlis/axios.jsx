7af00fa929e177c0c0bd48e4840082ab67349868
Andy Joseph <neon_4@hotmail.com>
2024-11-07 08:21:32 +0000

    lose axios timeout

--- client/src/utlis/axios.jsx
+++ client/src/utlis/axios.jsx
@@ -7,7 +7,7 @@ import authStore from "../stores/authStore";
 const mainURL = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true,
-    timeout: 5000,
+    // timeout: 5000,
     
     // headers: {
     //     "Access-Control-Allow-Origin" : "",
@@ -15,7 +15,7 @@ const mainURL = axios.create({
     //     "Authorization": `Bearer ${store.token}`,
     //     "Accept-Encoding": "*",
         
-    //     }   
+    //     }    s
 
 })
 
